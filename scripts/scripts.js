ade95523ae1e54103bb196ab23253659bdd6da54
Adeola Fadare <adeolafadare8@gmail.com>
2022-08-19 11:56:39 +0100

    done,now working on form validation

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -30,31 +30,41 @@ class formValidator {
 
     validateFields(field) {
         if (field.value.trim() == "") {
-            this.setStatus(field, `${(field.previousElementSibling.previousElementSibling.innerText).toLowerCase()} cannot be left empty`, 'error')
+            this.setStatus(field, `${(field.previousElementSibling.innerText).toLowerCase()} is required`, 'error')
         }
         else {
             if (field.id == 'email') {
-                const re = /\S+@\S+./
+                var re = /\S+@\S+./
                 if (re.test(field.value)) {
                     this.setStatus(field, null, 'success')
                 }
-                else { this.setStatus(field, 'enter a valid email', 'error') }
+                else { this.setStatus(field, '*enter a valid email', 'error') }
             }
             else if (field.id == 'password') {
+                var re = /(([A-Z]+)([0-9]+)([a-z]+))|(([0-9]+)([A-Z]+)([a-z]+))|(([0-9]+)([a-z]+)([A-Z]+))|(([A-Z]+)([a-z]+)([0-9]+))|(([a-z]+)([A-Z]+)([0-9]+))|(([a-z]+)([0-9]+)([A-Z]+))/
+                if (re.test(field.value)) {
+                    this.setStatus(field, null, 'success')
+                }
+                else {
+                    console.log('no')
+                    this.setStatus(field, '*must contain at least one lowercase, uppercase and digit', 'error')
+                }
                 if (field.value.length >= 6 && field.value != '123456') {
                     this.setStatus(field, null, 'success')
                 }
                 else {
-                    this.setStatus(field, 'minimum of 6 and cannot be 123456', 'error')
+                    this.setStatus(field, '*minimum of 6 and cannot be 123456', 'error')
                 }
-                if (this.form.querySelector('#password_confirmation').value != field.value) {
-                    this.setStatus(this.form.querySelector('#password_confirmation'), 'password match required', 'error')
-                }
-                else { this.setStatus(this.form.querySelector('#password_confirmation'), null, 'success') }
+                try {
+                    if (this.form.querySelector('#password_confirmation').value != field.value) {
+                        this.setStatus(this.form.querySelector('#password_confirmation'), '*password do not match', 'error')
+                    }
+                    else { this.setStatus(this.form.querySelector('#password_confirmation'), null, 'success') }
+                } catch { }
             }
             else if (field.id == 'confirm-password') {
                 if (field.value != this.form.querySelector('#password').value) {
-                    this.setStatus(field, 'password match required', 'error')
+                    this.setStatus(field, '*password do not match', 'error')
 
                 }
                 else { this.setStatus(this.form.querySelector('#password_confirmation'), null, 'success') }
@@ -71,7 +81,12 @@ class formValidator {
 
     setStatus(field, message, status) {
         field.parentElement.querySelector('#error').innerText = message
-
+        if (status == 'error') {
+            field.parentElement.classList.add('error')
+        }
+        else {
+            field.parentElement.classList.remove('error')
+        }
     }
 
 }
@@ -82,3 +97,5 @@ const fields = ['first-name', 'last-name', 'email', 'password', 'confirm-passwor
 const validator = new formValidator(form, fields)
 
 validator.initialise()
+var reg = /@|([0-9])/g  //\S+@\S+./
+console.log([...'2Sj@]1234,saad'.match(reg)]);
